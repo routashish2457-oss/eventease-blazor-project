d8f67555b7eea67944460fb13ad8d54434668d8f
routashish2457-oss <routashish2457@gmail.com>
2026-01-08 00:52:52 +0530

    Initialize Blazor WebAssembly app with services

---
+++ program.cs
@@ -0,0 +1,11 @@
+using EventEase.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddScoped<EventService>();
+builder.Services.AddScoped<SessionService>();
+
+await builder.Build().RunAsync();
